0882f70868ffd30275c020e3fd5e86c78cbdfa81
= <=>
2017-05-06 13:20:54 +0600

    added style.scss and trying to integrate it

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 window._ = require('lodash');
-
 window.$ = window.jQuery = require('jquery');
 
-window.models = require('./models/index');
-
+require('bootstrap-sass');
 require('bootstrap');
 
-require('./scripts/loaded');
+require('./scripts/onload');
 
-
+window.models = require('./models/index');
--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,47 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	 entry: './src/app.js',
-	 output: {
+	context: __dirname,
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
 		path: path.join(__dirname, '.bin'),
-		filename: 'app.min.js'
-	 },
-	 module: {
+		filename: '[name].min.js'
+	},
+	module: {
 		loaders: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
 			loader: 'babel-loader'
 		}, {			
-    		test: /\.ejs$/,
-    		loader: "ejs-loader"
+			test: /\.ejs$/,
+			loader: "ejs-loader"
+		}, {
+			test: /\.json$/,
+			use: 'json-loader'
+		}, {
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+			  fallback: 'style-loader',
+			  //resolve-url-loader may be chained before sass-loader if necessary
+			  use: ['css-loader', 'sass-loader']
+			})
 		}]
 	},
 	plugins: [
 		new WebpackCleanupPlugin(),
+		new ExtractTextPlugin("style.css"),
 		new HtmlWebpackPlugin({
-    		template: './src/template.js',
-    	}),
+			template: './src/template.js',
+			minify: require('html-minifier'),
+			chunks: {
+				css: [ 'style.css' ]
+			}
+		}),
 		new webpack.optimize.UglifyJsPlugin({
 			compress: {
 				warnings: false,
@@ -34,10 +53,10 @@ module.exports = {
 			},
 		}),
 		new CopyWebpackPlugin([{
-			from: path.join(__dirname, 'public'),
-			to: 'public'
+			from: path.join(__dirname, 'static'),
+			to: 'static'
 		}], {
-            ignore: [ '.*' ]
-        }),
+			ignore: [ '.*' ]
+		})
 	]
 };
