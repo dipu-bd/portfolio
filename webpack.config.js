491a5e1cfdf645a504d4635adf2e9069d5c1207c
= <=>
2017-05-08 06:50:28 +0600

    using .filter to separated production/development plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const _ = require('lodash');
 const path = require('path');
 const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var release = (process.env.NODE_ENV === 'production');
+const development = (process.env.NODE_ENV !== 'production');
 
 module.exports = {
 	entry: {
 		app: './src/app.js'
 	},
 	output: {
-		path: path.join(__dirname, release ? 'dist' : '.bin'),
+		path: path.join(__dirname, ['dist', '.bin'][development ? 1 : 0]),
 		filename: '[name].min.js'
+	},
+	devServer: {
+		contentBase: './bin',
+		inline: true,
+		port: 8000,
+		open: false
 	},
 
 	module: {		
@@ -25,18 +32,24 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader'
-			}, 
-			// compiles ejs files into html
-			{			
-				test: /\.ejs$/,
-				loader: "ejs-loader"
-			}, 
-			// copy all static resources into single svg file
-			// note: url-loader useds file-loader
-			{ 
-				test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/, 
-				loader: 'url-loader?limit=100000' 
-			}, 
+			},
+			// compiles ejs files into html. 
+			// ejs-loader can be used as alternative.
+            { 
+            	test: /\.(html|ejs)$/,
+            	loader: "underscore-template-loader"
+        	},
+        	// use file-loader to load assets. require() will return a filename
+            {
+            	test: /\.(jpg|png|bmp)$/, 
+            	loader: "file-loader" 
+            },
+            // replace path mentioned inside url() function
+			// url-loader uses file-loader
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'url-loader?limit=100000'
+			},
 			// compiles scss files using sass-loader
 			{
 				test: /\.(css|scss)$/,
@@ -51,49 +64,45 @@ module.exports = {
 				})
 			}
 		]
-	}
-};
-
-var plugins = [
-	//new WebpackCleanupPlugin(),	
-	new ExtractTextPlugin({
-		filename: "[name].min.css",
-		//disable: !release // extract only in production
-	}),
-	new CopyWebpackPlugin([{
-		from: path.join(__dirname, 'static'),
-		to: 'static'
-	}], {
-		ignore: [ 			
-			'.*', // ignore files starting with a dot
-		],
-		copyUnmodified: false
-	}),
-	new HtmlWebpackPlugin({
-		template: './src/index.ejs',
-		minify: release && {
-			collapseWhitespace: true,
-			removeComments: true,
-			removeRedundantAttributes: true,
-			removeScriptTypeAttributes: true,
-			removeStyleLinkTypeAttributes: true
-		}
-	}),	
-];
-
-// plugins to load only for release profile
-if(release) {
-	plugins = _.concat(plugins, [
+	},
+	plugins: [
+		new WebpackCleanupPlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
 		new OptimizeCssAssetsPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: true,
-			},
-			output: {
-				comments: false,
-			},
+		new ExtractTextPlugin({
+			filename: "[name].min.css",
 		}),
-	])
+		new HtmlWebpackPlugin({
+			template: './src/index.ejs',
+			minify: (!development) && {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true
+			}
+		}),
+		/*
+		new CopyWebpackPlugin([{
+			from: path.join(__dirname, 'static'),
+			to: 'static'
+		}], {
+			ignore: [ 			
+				'.*', // ignore files starting with a dot
+			],
+			copyUnmodified: false
+		}),
+		*/
+	].filter(function(plugin) {
+		const disabled = development ? [
+			'UglifyJsPlugin',
+			'OptimizeCssAssetsPlugin',
+		] : [
+			'WebpackCleanupPlugin',
+		];		
+		const canuse = disabled.indexOf(plugin.constructor.name) < 0;
+		canuse && console.log(">> plugin: " + plugin.constructor.name);		
+		return canuse;
+	})
 }
 
-module.exports.plugins = plugins;
