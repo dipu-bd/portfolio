f4e79198dc53b4d41336174cdfea06c3d445eae7
dipu-bd <dipu.sudipta@gmail.com>
2017-05-18 00:29:18 +0600

    Projects changed

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -4,14 +4,30 @@ var Projects = require('../data/Projects.json');
 //
 // Enhance module
 //
-
+Projects.types = extractTypes();
+Projects.sortedTypes = sortedTypes();
 
 //
 // Export the modules
 //
 module.exports = Projects;
 
-
 //
 // Private functions
 //
+function extractTypes()
+{
+	let types = {};
+	_.forIn(Projects, function(project, key) {
+		_.each(project.types, function(type) {
+			types[type] = types[type] || [];
+			types[type].push(key);
+		});
+	});
+	return types;
+}
+
+function sortedTypes() {
+	let types = extractTypes();
+	return _.sortBy(_.keys(types), k => -(types[k].length));
+}
