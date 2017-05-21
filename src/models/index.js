0fb2b1780c3fc5614e14574af5b2ad086b5a4ed1
dipu-bd <dipu.sudipta@gmail.com>
2017-05-21 23:18:58 +0600

    models/index.js: added ProjectInfo

--- src/models/index.js
+++ src/models/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	Website: require('./Website'),
 	Personal: require('./Personal'),
 	Skills: require('./Skills'),
-	Projects: require('./Projects')
+	Projects: require('./Projects'),
+	ProjectInfo: require('./ProjectInfo')
 };
